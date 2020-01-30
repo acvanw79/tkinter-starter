674a70ab573ec5bb3c29462fc5af528b854f4087
Adam VanWyk <60404099+acvanw79@users.noreply.github.com>
2020-01-30 14:33:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 # A starter program for Python with Tkinter
 
 from tkinter import * # import Tkinter library
+from tkinter.ttk import *
+from tkinter import Menu
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
 lbl = Label(window, text="Once upon a time, in a galaxy far far away....")
 
 # Place the label in the window at 0, 0
-lbl.grid(column=0, row=0)
+lbl.grid(column=0, row=5)
+txt = Entry(window,width=40)
+ 
+txt.grid(column=1, row=0)
+firstLabel = Label(window, text="Hello", font=("Arial Bold", 50))
+firstLabel.grid(column=0, row=0) 
 
+ 
+txt.grid(column=1, row=0)
+
+cnk = Checkbutton(window, text='Choose')
+window.title("Welcome to my hour and a half")
+window.geometry('540x300')
+chk_state = BooleanVar()
+chk_state.set(True) #set check state
+chk = Checkbutton(window, text='pick', var=chk_state)
+chk.grid(column=2, row=5)
+
+
+
+menu = Menu(window)
+
+window = Tk()
+window.title("Wndo #2")
+
+new_item = Menu(menu)
+new_item.add_command(label='New boy')
+menu.add_command(label='File')
+window.config(menu=menu)
+menu.add_cascade(label='File', menu=new_item)
+window.config(menu=menu)
 
 window.mainloop()     # Keep the window open
+
+
+
+
+
+
+
